1673026880882aa1a285100cdf454fc57b11776c
Sahana Balram <sahana2008balram@gmail.com>
2018-05-16 17:03:52 -0700

    Import stacknavigator and mainscreen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { StackNavigator } from 'react-navigation'
-import MainScreen from './Components/MainScreen'
+import { StackNavigator } from 'react-navigation';
+import MainScreen from './Components/MainScreen';
 
 export default class App extends React.Component {
   render() {
